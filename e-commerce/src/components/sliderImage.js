5482913f96e1519c9d2ef3335c50c325fa69c875
Diana Palafox <dianapalafox95@gmail.com>
2023-02-13 14:19:30 -0700

    Create the list of carousel data

---
+++ e-commerce/src/components/sliderImage.js
@@ -0,0 +1,30 @@
+import First from '../images/img1.jpeg';
+import Second from '../images/img2.jpeg';
+import Third from '../images/img3.jpeg';
+import Four from '../images/img4.jpeg';
+import Five from '../images/img5.jpeg';
+
+const SliderImage = [
+    {
+        title: 'First Slide',
+        urls: First,
+    },
+    {
+        title: 'Second Slide',
+        urls: Second,
+    },
+    {
+        title: 'Third Slide',
+        urls: Third,
+    },
+    {
+        title: 'Four Slide',
+        urls: Four,
+    },
+    {
+        title: 'Five Slide',
+        urls: Five,
+    },
+];
+
+export default SliderImage;
